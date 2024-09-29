d99a37d086b0fb57340a204d37b28bf678a0df80
baotroy <baotroy@users.noreply.github.com>
2024-09-29 23:14:16 +0700

    Add ticket booking functionality

--- main.go
+++ main.go
@@ -3,9 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to example")
+	var conferanceName string = "Go Conference"
 
-	var name  = "John Doe"
-	fmt.Println("Hello", name)
+	const conferanceTikets int = 50
+	var remainingTickets int = conferanceTikets
 
+	fmt.Printf("conferenceTickets is %T, rmainingTickets is %T, conferenceName is %T \n", conferanceTikets, remainingTickets, conferanceName)
+	fmt.Printf("Welcome to %v booking application\n", conferanceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferanceTikets, remainingTickets)
+
+	fmt.Println("Get your ticket here")
+
+	var userName  string
+	var lastName string
+	var email string
+	var userTickets int
+	fmt.Println("Enter your first name")
+	fmt.Scan(&userName)
+
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email")
+	fmt.Scan(&email)
+	
+
+	// userName = "John Doe"
+	userTickets = 2
+	fmt.Printf("%s booked %d tickets\n", userName, userTickets)
 }
